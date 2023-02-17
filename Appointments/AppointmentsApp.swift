5fd587f8737dbafdfaf5887687f310633317d211
Peter Alt <peter@peteralt.com>
2023-02-17 09:45:16 -0700

    Initial Commit

---
+++ Appointments/AppointmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppointmentsApp.swift
+//  Appointments
+//
+//  Created by Peter Alt on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AppointmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
