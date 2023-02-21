57ee2cc852cb527229b40a38e3418276c77c874e
Peter Alt <peter@peteralt.com>
2023-02-21 09:58:38 -0700

    cleaning up and restructuring

--- Appointments/AppointmentDetailFeature/AppointmentDetailFeature+View.swift
+++ Appointments/AppointmentDetailFeature/AppointmentDetailFeature+View.swift
@@ -27,55 +27,6 @@ extension Appointment.Status {
     }
 }
 
-struct AppointmentDetailFeature: ReducerProtocol {
-    
-    struct State: Equatable, Identifiable {
-        var id: String { appointment.id }
-        var appointment: Appointment
-        
-        var name: String {
-            appointment.user.firstName + " " + appointment.user.lastName
-        }
-        
-        var initials: String {
-            return name.components(separatedBy: " ")
-                .reduce("") {
-                    ($0.isEmpty ? "" : "\($0.first?.uppercased() ?? "")") +
-                    ($1.isEmpty ? "" : "\($1.first?.uppercased() ?? "")")
-                }
-        }
-        
-        var displayInitials: Bool {
-            return appointment.status != .readyToJoin && appointment.status != .responseRequired
-        }
-    }
-    
-    enum Action: Equatable {
-        case didTapAccept
-        case didTapDecline
-        case didTapJoinAppointment
-    }
-    
-    var body: some ReducerProtocol<State, Action> {
-        Reduce { state, action in
-            switch action {
-                
-            case .didTapAccept:
-                state.appointment.status = .active
-                return .none
-                
-            case .didTapDecline:
-                state.appointment.status = .completed
-                return .none
-                
-            case .didTapJoinAppointment:
-                // we would do something here, but it's out of scope
-                return .none
-            }
-        }
-    }
-}
-
 struct AppointmentDetailView: View {
     let store: StoreOf<AppointmentDetailFeature>
     
--- Appointments/AppointmentListsFeature/AppointmentListFeature+View.swift
+++ Appointments/AppointmentListsFeature/AppointmentListFeature+View.swift
@@ -1,59 +1,6 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct AppointmentListFeature: ReducerProtocol {
-    @Dependency(\.apiClient) var apiClient
-    private enum APIRequestCancellationID {}
-    
-    struct State: Equatable {
-        var appointments: IdentifiedArrayOf<AppointmentDetailFeature.State> = []
-        
-        /// Displays a loading state indicator
-        var isLoading: Bool = false
-    }
-    
-    enum Action: Equatable {
-        case didAppear
-        case appointments(TaskResult<AppointmentResponse>)
-        case appointment(id: AppointmentDetailFeature.State.ID, action: AppointmentDetailFeature.Action)
-    }
-    
-    var body: some ReducerProtocol<State, Action> {
-        Reduce { state, action in
-            switch action {
-            case .didAppear:
-                state.isLoading = true
-                return .task {
-                    return await .appointments(TaskResult {
-                        try await apiClient
-                            .fetchAppointments()
-                    })
-                }
-                .cancellable(id: APIRequestCancellationID.self)
-                
-            case let .appointments(.success(response)):
-                state.isLoading = false
-                let appointments = response.appointments
-                    .map { AppointmentDetailFeature.State(appointment: $0) }
-                state.appointments = .init(uniqueElements: appointments)
-                return .none
-                
-            case .appointments(.failure):
-                // We're not handling any errors right now, we could capture
-                // the error here and display an error message in the future.
-                state.isLoading = false
-                return .none
-                                
-            case .appointment:
-                return .none
-            }
-        }
-        .forEach(\.appointments, action: /Action.appointment(id:action:)) {
-            AppointmentDetailFeature()
-        }
-    }
-}
-
 struct AppointmentsListView: View {
     let store: StoreOf<AppointmentListFeature>
     
